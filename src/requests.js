4ae7e5e34e2caade7dcc04bb9d80875e94d6d2a2
Samir Malek <41745264+samirmalek-dev@users.noreply.github.com>
2020-10-06 01:29:17 +0300

    First Commit Done

---
+++ src/requests.js
@@ -0,0 +1,9 @@
+const API_KEY = "cdc44439cd52dd81cbc02b1ed58165b2";
+
+const requests = {
+    fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en-US',
+    fetchNetflixOriginals: '/discover/tv?api_key=${API_KEY}&with_networks=213',
+    
+
+}
+
