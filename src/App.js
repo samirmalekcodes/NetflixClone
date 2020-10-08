4bfdadaac4f74d10d824abc1e041f523142936ff
Samir Malek <41745264+samirmalek-dev@users.noreply.github.com>
2020-10-08 14:57:05 +0300

    added api_keys

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Hey Samir Malek , Let's build the Netflix App </h1>
-    </div>
+  return ( <
+    div className = "App" >
+    <
+    h1 > Hey Samir Malek, Let 's build the Netflix App </h1> <
+    /div>
   );
 }
 
